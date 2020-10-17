4b5ba2789ce3737b91d995524a3bc09973f123a4
klasycznienielogiczny <ppaluch@student.agh.edu.pl>
2020-10-17 16:52:58 +0200

    Podejście numer piatee ;)

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main() {
     printf("Hello, World!\n");
     printf("Czy sie udalo czy sie nie udalo?\n");
     printf("A teraz?\n");
+    printf("No dobra. Zgubilem w sumie trzeci commit gdzies, ale juz wiem przynajmniej jak to dziala\n");
     return 0;
 }
