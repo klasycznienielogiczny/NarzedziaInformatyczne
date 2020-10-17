d99bd9168bf5ac221079fb39fc305e9e1c8b2199
klasycznienielogiczny <ppaluch@student.agh.edu.pl>
2020-10-17 16:15:16 +0200

    pierwszy commit

---
+++ cmake-build-debug/CMakeFiles/NInf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NInf.dir/main.c.obj"
+  "NInf.exe"
+  "NInf.exe.manifest"
+  "NInf.pdb"
+  "libNInf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NInf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
